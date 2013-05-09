21fbaa9de3a40015696afaf91cb9476dbda0b6a6
her01@live.com.ar <her01@live.com.ar@717ebda4-37a0-fc2c-3e47-55d0704b0d64>
2013-05-09 16:14:52 +0000

    Funciones para el analizador

--- trunk/Analizador léxico/Analizador léxico/AnalizadorLexico.c
+++ trunk/Analizador léxico/Analizador léxico/AnalizadorLexico.c
@@ -29,30 +29,50 @@ void finalizarId(tokenAAnalizar *tokenActual, char caracter)
 //	insertarEnTS(tokenActual,ID);
 }
 
-//CTE_NUMERICA 
+//CTE_ENTERA o CTE_REAL 
+ 
 
 
-void iniciarCteNum(tokenAAnalizar *tokenActual, char caracter)
+void iniciarCteEnteraOReal(tokenAAnalizar *tokenActual, char caracter)
 {
 	tokenActual->token[tokenActual->longitudToken] = caracter;
 	tokenActual->longitudToken++;
 	tokenActual->token[tokenActual->longitudToken] = '\0';
+}
+
+void continuarCteEnteraOReal(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++ ;
+	tokenActual->token[tokenActual->longitudToken] = '\0';
+}
+
+void finalizarCteEntera(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,CTE_ENTERA);
 
 }
 
-void continuarCteNum(tokenAAnalizar *tokenActual, char caracter)
+void iniciarCteReal(tokenAAnalizar *tokenActual, char caracter)
 {
 	tokenActual->token[tokenActual->longitudToken] = caracter;
 	tokenActual->longitudToken++;
 	tokenActual->token[tokenActual->longitudToken] = '\0';
-
 }
 
-void finalizarCteNum(tokenAAnalizar *tokenActual, char caracter)
+void continuarCteReal(tokenAAnalizar *tokenActual, char caracter)
 {
-	//insertarEnTS(tokenActual,CTE_NUMERICA) ;
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0';
+}
+
+void finalizarCteReal(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,CTE_REAL);
 
 }
+
 
 //CTE_STRING 
 
@@ -73,12 +93,9 @@ void continuarCteStr(tokenAAnalizar *tokenActual, char caracter)
 
 void finalizarCteStr(tokenAAnalizar *tokenActual, char caracter)
 {
-tokenActual->token[tokenActual->longitudToken] = caracter;
-tokenActual->longitudToken++;
-tokenActual->token[tokenActual->longitudToken] = '\0';
-//insertarEnTS(tokenActual,CTE_STRING);
-//descartar = false; 
 
+	//insertarEnTS(tokenActual,CTE_STRING);
+	//cteStringAbierta = FALSE; 
 }
 
 //OP_SUMA u OP_CONCATENACION 
@@ -108,3 +125,276 @@ void finalizarOpConcat(tokenAAnalizar *tokenActual, char caracter)
 
 }
 
+//OP_RESTA o comentario 
+
+void iniciarOpRestaOComent(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0';
+}
+
+void finalizarOpResta(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,OP_RESTA);
+}
+
+
+void continuarComentario(tokenAAnalizar *tokenActual, char caracter)
+{
+	//comentarioAbierto = TRUE;
+}
+
+void finalizarComentario(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->longitudToken = 0;
+	//comentarioAbierto = FALSE;
+
+}
+
+//OP_MULTIPLICACION 
+
+void iniciarOpMulti(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0';
+
+}
+void finalizarOpMulti(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,OP_MULTIPLICACION); 
+
+}
+
+//OP_DIVISION 
+void iniciarOpDivi(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+
+}
+void finalizarOpDivi(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,OP_DIVISION);
+
+}
+
+//PARENTESIS_ABRE 
+void iniciarParAbre(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0';
+}
+
+void finalizarParAbre(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,PARENTESIS_ABRE) ;
+}
+
+//PARENTESIS_CIERRA 
+void iniciarParCierra(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0';
+
+}
+void finalizarParCierra(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,PARENTESIS_CIERRA) ;
+}
+
+//CORCHETES_ABRE 
+void iniciarCorAbre(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+}
+
+void finalizarCorAbre(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,CORCHETES_ABRE);
+}
+
+//CORCHETES_CIERRA 
+void iniciarCorCierra(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0';
+
+}
+
+void finalizarCorCierra(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,CORCHETES_CIERRA);
+}
+
+//PUNTO_COMA 
+void iniciarPuntoComa(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+}
+
+void finalizarPuntoComa(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,PUNTO_COMA) ;
+}
+
+//DOS_PUNTOS 
+
+void iniciarDosPuntos(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+}
+
+void finalizarDosPuntos(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,DOS_PUNTOS) ;
+}
+
+//GUION_BAJO 
+void iniciarGuionBajo(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0';
+
+
+}
+
+void finalizarGuionBajo(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,GUION_BAJO);
+
+}
+
+//COMA 
+void iniciarComa(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+}
+
+void finalizarComa(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,COMA);
+}
+
+//OP_MENOR u OP_MENOR_IGUAL 
+void iniciarOpMenorOMenorIgual(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+}
+
+void finalizarOpMenor(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,OP_MENOR);
+}
+
+void continuarOpMenorIgual(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+}
+
+void finalizarOpMenorIgual(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,OP_MENOR_IGUAL);
+}
+
+//OP_MAYOR u OP_MAYOR_IGUAL 
+
+void iniciarOpMayorOMayorIgual(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+}
+void finalizarOpMayor(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,OP_MAYOR);
+}
+
+void continuarOpMayorIgual(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+}
+
+void finalizarOpMayorIgual(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,OP_MAYOR_IGUAL) ;
+}
+
+//OP_ASIGNACION u OP_IGUAL 
+void iniciarOpAsigOIgual(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+}
+
+void finalizarOpAsig(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,OP_ASIGNACION); 
+}
+
+void continuarOpIgual(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+
+}
+void finalizarOpIgual(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,OP_IGUAL);
+
+}
+
+//OP_DISTINTO 
+
+void iniciarOpDistinto(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+}
+
+void continuarOpDistinto(tokenAAnalizar *tokenActual, char caracter)
+{
+	tokenActual->token[tokenActual->longitudToken] = caracter;
+	tokenActual->longitudToken++;
+	tokenActual->token[tokenActual->longitudToken] = '\0'; 
+}
+
+void finalizarOpDistinto(tokenAAnalizar *tokenActual, char caracter)
+{
+	//insertarEnTS(tokenActual,OP_DISTINTO); 
+
+}
+
+//Espacios vacíos (ENTER, TAB, ESPACIO) 
+void ignorarEspacios(tokenAAnalizar * (tokenActual), char caracter)
+{
+
+}
+
+//Error de sintáxis 
+void syntaxError(tokenAAnalizar * (tokenActual), char caracter)
+{
+}
--- trunk/Analizador léxico/Analizador léxico/AnalizadorLexico.h
+++ trunk/Analizador léxico/Analizador léxico/AnalizadorLexico.h
@@ -65,72 +65,109 @@ void iniciarId(tokenAAnalizar *tokenActual, char caracter);
 void continuarId(tokenAAnalizar *tokenActual, char caracter);
 void finalizarId(tokenAAnalizar *tokenActual, char caracter);
 
-void iniciarCteNum(tokenAAnalizar *tokenActual, char caracter);
-void continuarCteNum(tokenAAnalizar *tokenActual, char caracter);
-void finalizarCteNum(tokenAAnalizar *tokenActual, char caracter);
+//CTE_ENTERA o CTE_REAL
+void iniciarCteEnteraOReal(tokenAAnalizar *tokenActual, char caracter);
+void continuarCteEnteraOReal(tokenAAnalizar *tokenActual, char caracter);
+void finalizarCteEntera(tokenAAnalizar *tokenActual, char caracter);
+void iniciarCteReal(tokenAAnalizar *tokenActual, char caracter);
+void continuarCteReal(tokenAAnalizar *tokenActual, char caracter);
+void finalizarCteReal(tokenAAnalizar *tokenActual, char caracter);
+
+
+
+
 
 void iniciarCteStr(tokenAAnalizar *tokenActual, char caracter);
 void continuarCteStr(tokenAAnalizar *tokenActual, char caracter);
 void finalizarCteStr(tokenAAnalizar *tokenActual, char caracter);
+
+
+
 
 void iniciarOpSumaOConcat(tokenAAnalizar *tokenActual, char caracter);
 void finalizarOpSuma(tokenAAnalizar *tokenActual, char caracter);
 void continuarOpConcat(tokenAAnalizar *tokenActual, char caracter);
 void finalizarOpConcat(tokenAAnalizar *tokenActual, char caracter);
 
+
+//OP_RESTA o comentario 
 void iniciarOpRestaOComent(tokenAAnalizar *tokenActual, char caracter);
 void finalizarOpResta(tokenAAnalizar *tokenActual, char caracter);
 void continuarComentario(tokenAAnalizar *tokenActual, char caracter);
 void finalizarComentario(tokenAAnalizar *tokenActual, char caracter);
 
+//OP_MULTIPLICACION 
 void iniciarOpMulti(tokenAAnalizar *tokenActual, char caracter);
 void finalizarOpMulti(tokenAAnalizar *tokenActual, char caracter);
 
+//OP_DIVISION 
 void iniciarOpDivi(tokenAAnalizar *tokenActual, char caracter);
 void finalizarOpDivi(tokenAAnalizar *tokenActual, char caracter);
 
+//PARENTESIS_ABRE 
 void iniciarParAbre(tokenAAnalizar *tokenActual, char caracter);
 void finalizarParAbre(tokenAAnalizar *tokenActual, char caracter);
 
+//PARENTESIS_CIERRA 
 void iniciarParCierra(tokenAAnalizar *tokenActual, char caracter);
 void finalizarParCierra(tokenAAnalizar *tokenActual, char caracter);
 
+//CORCHETES_ABRE 
 void iniciarCorAbre(tokenAAnalizar *tokenActual, char caracter);
 void finalizarCorAbre(tokenAAnalizar *tokenActual, char caracter);
 
+//CORCHETES_CIERRA 
 void iniciarCorCierra(tokenAAnalizar *tokenActual, char caracter);
 void finalizarCorCierra(tokenAAnalizar *tokenActual, char caracter);
 
+//PUNTO_COMA 
 void iniciarPuntoComa(tokenAAnalizar *tokenActual, char caracter);
 void finalizarPuntoComa(tokenAAnalizar *tokenActual, char caracter);
 
+//DOS_PUNTOS 
 void iniciarDosPuntos(tokenAAnalizar *tokenActual, char caracter);
 void finalizarDosPuntos(tokenAAnalizar *tokenActual, char caracter);
 
+//GUION_BAJO 
 void iniciarGuionBajo(tokenAAnalizar *tokenActual, char caracter);
 void finalizarGuionBajo(tokenAAnalizar *tokenActual, char caracter);
 
+//COMA 
 void iniciarComa(tokenAAnalizar *tokenActual, char caracter);
 void finalizarComa(tokenAAnalizar *tokenActual, char caracter);
 
+//OP_MENOR u OP_MENOR_IGUAL 
 void iniciarOpMenorOMenorIgual(tokenAAnalizar *tokenActual, char caracter);
 void finalizarOpMenor(tokenAAnalizar *tokenActual, char caracter);
 void continuarOpMenorIgual(tokenAAnalizar *tokenActual, char caracter);
 void finalizarOpMenorIgual(tokenAAnalizar *tokenActual, char caracter);
 
+//OP_MAYOR u OP_MAYOR_IGUAL 
 void iniciarOpMayorOMayorIgual(tokenAAnalizar *tokenActual, char caracter);
 void finalizarOpMayor(tokenAAnalizar *tokenActual, char caracter);
 void continuarOpMayorIgual(tokenAAnalizar *tokenActual, char caracter);
 void finalizarOpMayorIgual(tokenAAnalizar *tokenActual, char caracter);
 
+//OP_ASIGNACION u OP_IGUAL 
 void iniciarOpAsigOIgual(tokenAAnalizar *tokenActual, char caracter);
 void finalizarOpAsig(tokenAAnalizar *tokenActual, char caracter);
 void continuarOpIgual(tokenAAnalizar *tokenActual, char caracter);
 void finalizarOpIgual(tokenAAnalizar *tokenActual, char caracter);
 
+//OP_DISTINTO 
 void iniciarOpDistinto(tokenAAnalizar *tokenActual, char caracter);
 void continuarOpDistinto(tokenAAnalizar *tokenActual, char caracter);
 void finalizarOpDistinto(tokenAAnalizar *tokenActual, char caracter);
+
+
+//Espacios vacíos (ENTER, TAB, ESPACIO) 
+void ignorarEspacios(tokenAAnalizar * (tokenActual), char caracter);
+
+//Error de sintáxis 
+void syntaxError(tokenAAnalizar * (tokenActual), char caracter);
+	
+
+
 
 /*Matriz de Nuevo Estado*/
 int nuevoEstado[ESTADO_FINAL][CANT_COLUMNAS];
